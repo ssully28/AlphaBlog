ce721e90915d3d7f388fd09968903f90abc8fa15
Steve Sullivan <steve.sullivan@berkeley.edu>
2020-03-12 13:06:12 -0700

    Added edit update show and index

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,9 @@
 class ArticlesController < ApplicationController
 
+  def index
+    @articles = Article.all
+  end
+
   def new
     # @article gets passed over to the view (new.html.erb)
     @article = Article.new
@@ -23,8 +27,25 @@ class ArticlesController < ApplicationController
     @article = Article.find(params[:id])
   end
 
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    
+    if @article.update(article_params)
+      flash[:notice] = "Article was successfully updated"
+      redirect_to article_path(@article)
+    else
+      render 'edit'
+    end
+
+  end
+
 
   private
+    # Whitelist parameters:
     def article_params
       params.require(:article).permit(:title, :description)
     end
